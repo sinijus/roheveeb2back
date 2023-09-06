8dc84b53174710707e4a20971c9ea0febb340acb
Jaanus <134766023+sinijus@users.noreply.github.com>
2023-09-06 18:57:00 +0300

    Initial commit

---
+++ src/main/java/ee/valiit/roheveeb2back/Roheveeb2backApplication.java
@@ -0,0 +1,13 @@
+package ee.valiit.roheveeb2back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Roheveeb2backApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Roheveeb2backApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/valiit/roheveeb2back/Roheveeb2backApplicationTests.java
@@ -0,0 +1,14 @@
+package ee.valiit.roheveeb2back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Roheveeb2backApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
